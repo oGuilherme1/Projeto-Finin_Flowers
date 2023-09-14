45fae245f44a8d19301242f7d60ffc4dfe3c65fd
Guilherme Gonçalves <guilhermegoncalves1314@gmail.com>
2023-09-13 22:25:30 -0300

    Subindo landingPage

--- example-app/resources/views/home.blade.php
+++ example-app/resources/views/home.blade.php
@@ -121,30 +121,23 @@
                 </div>
                 <div class="flex flex-wrap items-center mt-32">
                     <div class="w-full md:w-5/12 px-4 mr-auto ml-auto">
-                        <div class="text-gray-600 p-3 text-center inline-flex items-center justify-center w-16 h-16 mb-6 shadow-lg rounded-full bg-gray-100">
-                            <i class="fas fa-user-friends text-xl"></i>
-                        </div>
-                        <h3 class="text-3xl mb-2 font-semibold leading-normal">
-                            Working with us is a pleasure
+                        <h3 class="text-3xl mb-2 font-semibold leading-normal text-green-500">
+                            Sobre Nos
                         </h3>
-                        <p class="text-lg font-light leading-relaxed mt-4 mb-4 text-gray-700">
-                            Don't let your uses guess by attaching tooltips and popoves to
-                            any element. Just make sure you enable them first via
-                            JavaScript.
+                        <p class="text-lg font-light leading-relaxed mt-4 mb-4 text-gray-800">
+                        Bem-vindo à Finin Flowers, líder na produção legal de medicamentos à base de cannabis. Fundada com uma paixão inabalável pela saúde e o bem-estar, nossa empresa está comprometida em proporcionar soluções terapêuticas de alta qualidade, impulsionadas pela inovação e pela ciência, para melhorar a vida das pessoas em todo o mundo. 
                         </p>
-                        <p class="text-lg font-light leading-relaxed mt-0 mb-4 text-gray-700">
-                            The kit comes with three pre-built pages to help you get started
-                            faster. You can change the text and images and you're good to
-                            go. Just make sure you enable them first via JavaScript.
+                        <p class="text-lg font-light leading-relaxed mt-0 mb-4 text-gray-800">
+                        Nossa missão é tornar a cannabis medicinal acessível e segura para todos que necessitam dela, ao mesmo tempo em que promovemos a educação e a pesquisa contínuas para desmistificar os benefícios terapêuticos dessa planta extraordinária. Acreditamos que a cannabis tem o potencial de transformar a saúde e o bem-estar das pessoas, e estamos comprometidos em desempenhar um papel crucial nessa jornada.
                         </p>
                         <a href="https://www.creative-tim.com/learning-lab/tailwind-starter-kit#/presentation" class="font-bold text-gray-800 mt-8">Check Tailwind Starter Kit!</a>
                     </div>
                     <div class="w-full md:w-4/12 px-4 mr-auto ml-auto">
-                        <div class="relative flex flex-col min-w-0 break-words bg-white w-full mb-6 shadow-lg rounded-lg bg-pink-600">
-                            <img alt="..." src="https://images.unsplash.com/photo-1522202176988-66273c2fd55f?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=crop&amp;w=1051&amp;q=80" class="w-full align-middle rounded-t-lg" />
+                        <div class="relative flex flex-col min-w-0 break-words bg-white w-full mb-6 shadow-lg rounded-lg bg-green-500">
+                            <img alt="..." src="https://img.freepik.com/fotos-gratis/medico-segurando-a-folha-de-cannabis-e-uma-garrafa-de-oleo-de-cannabis-na-parede-branca_1150-26701.jpg?w=1380&t=st=1694653662~exp=1694654262~hmac=dfa88d18c3b5cd1becf8d4d23c02bfe0baee731d979f6dd721827b7df0b74a1b" />
                             <blockquote class="relative p-8 mb-4">
                                 <svg preserveAspectRatio="none" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 583 95" class="absolute left-0 w-full block" style="height: 95px; top: -94px;">
-                                    <polygon points="-30,95 583,95 583,65" class="text-pink-600 fill-current"></polygon>
+                                    <polygon points="-30,95 583,95 583,65" class="text-green-500 fill-current"></polygon>
                                 </svg>
                                 <h4 class="text-xl font-bold text-white">
                                     Top Notch Services
