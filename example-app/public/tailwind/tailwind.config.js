2c0d0ff140275a8a71374f290d339f980820e468
Guilherme Gonçalves <guilhermegoncalves1314@gmail.com>
2023-09-13 22:06:36 -0300

    Subindo landingPage

---
+++ example-app/public/tailwind/tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    theme: {
+      extend: {
+        colors: {
+          verde: '#648116',
+          customBlue: '#0000FF',
+          // Adicione mais cores personalizadas conforme necessário
+        },
+      },
+    },
+    variants: {},
+    plugins: [],
+  }
--- example-app/routes/web.php
+++ example-app/routes/web.php
@@ -13,6 +13,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('/home', function () {
+    return view('home');
+});
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::post('usuarios', ['uses' => 'ProdutoController@store', 'as' => 'produto']);
+
+
