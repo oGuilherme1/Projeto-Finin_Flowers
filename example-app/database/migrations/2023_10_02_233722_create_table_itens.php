5db3c6b7b86d410b56990424526129257ad98a9f
oTales <talesirineu@gmail.com>
2023-10-02 20:51:24 -0300

    adicionando migrate

--- example-app/database/migrations/2023_10_02_233722_create_table_itens.php
+++ example-app/database/migrations/2023_10_02_233722_create_table_itens.php
@@ -11,14 +11,14 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('table_itens', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
             $table->timestamps();
+            $table->string('nome', 100);
+            $table->foreignId('categoria_id')->constrained('table_categorias');
+            $table->decimal('valor');
+            $table->string('quantidade');
+            $table->enum('status', ['ativo', 'inativo'])->default('ativo');
         });
     }
 
@@ -27,6 +27,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('table_itens');
     }
 };
